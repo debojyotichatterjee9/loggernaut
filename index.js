13d38843268808c13bd13609aa65e5fedddf5e63
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2024-10-23 21:07:22 +0530

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+//index.js
+
+exports.info = (value) => {
+  console.log(new Date());
+  console.log(value)
+}
+
+exports.debug = (value) => {
+  console.log(new Date());
+  console.log(value)
+}
+
+exports.warn = (value) => {
+  console.log(new Date());
+  console.log(value)
+}
+
+exports.error = (value) => {
+  console.log(new Date());
+  console.log(value)
+}
+exports.trace = (value) => {
+  console.log(new Date());
+  console.log(value)
+}
