7be112117eeb49b3fa79e8c6a627060288fa9cbd
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2024-10-23 22:52:34 +0530

    name change

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 //index.js
 
+
+exports.log = (value) => {
+  console.log(value)
+}
 exports.info = (value) => {
   console.log(new Date());
-  console.log(value)
+  console.log(`INFO: ${value}`)
 }
 
 exports.debug = (value) => {
   console.log(new Date());
-  console.log(value)
+  console.log(`DEBUG: ${value}`)
 }
 
 exports.warn = (value) => {
   console.log(new Date());
-  console.log(value)
+  console.log(`WARN: ${value}`)
 }
 
 exports.error = (value) => {
   console.log(new Date());
-  console.log(value)
+  console.log(`ERROR: ${value}`)
 }
 exports.trace = (value) => {
   console.log(new Date());
-  console.log(value)
+  console.log(`TRACE: ${value}`)
 }
