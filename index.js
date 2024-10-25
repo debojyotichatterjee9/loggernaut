477386338024615bf08b0fe480f00d2c45cfa8e4
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2024-10-25 22:09:28 +0530

    ability to pass configs need to work on more options

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-//index.js
+// logger.js
 
+class Loggernaut {
+  constructor(config = {}) {
+    this.timestampFormat = config.timestampFormat || 'DD-MM-YYYY HH:mm:ss';
+  }
 
-exports.log = (value) => {
-  console.log(value)
-}
-exports.info = (value) => {
-  console.log(new Date());
-  console.log(`INFO: ${value}`)
+  getCurrentTimestamp() {
+    const date = new Date();
+    return this.formatDate(date, this.timestampFormat);
+  }
+
+  formatDate(date, format) {
+    const map = {
+      'YYYY': date.getFullYear(),
+      'MM': ('0' + (date.getMonth() + 1)).slice(-2),
+      'DD': ('0' + date.getDate()).slice(-2),
+      'HH': ('0' + date.getHours()).slice(-2),
+      'mm': ('0' + date.getMinutes()).slice(-2),
+      'ss': ('0' + date.getSeconds()).slice(-2),
+    };
+
+    return format.replace(/YYYY|MM|DD|HH|mm|ss/gi, matched => map[matched]);
+  }
+
+  log(message, customMessage = 'LOGGERNAUT') {
+    console.log(`${customMessage ? customMessage + '-->' : ''}${message}`);
+  }
+
+  info(message) {
+    console.log(`%cINFO --> ${this.getCurrentTimestamp()} --> ${message}`, 'color: cyan');
+  }
+
+  debug(message) {
+    console.log(`DEBUG --> ${this.getCurrentTimestamp()} --> ${message}`);
+  }
+
+  warn(message) {
+    console.log(`%cWARN --> ${this.getCurrentTimestamp()} --> ${message}`, 'color: orange');
+  }
+
+  error(message) {
+    console.log(`%cERROR --> ${this.getCurrentTimestamp()} --> ${message}`, 'color: red');
+  }
+
+  trace(message) {
+    console.log(`TRACE --> ${this.getCurrentTimestamp()} --> ${message}`);
+  }
 }
 
-exports.debug = (value) => {
-  console.log(new Date());
-  console.log(`DEBUG: ${value}`)
-}
-
-exports.warn = (value) => {
-  console.log(new Date());
-  console.log(`WARN: ${value}`)
-}
-
-exports.error = (value) => {
-  console.log(new Date());
-  console.log(`ERROR: ${value}`)
-}
-exports.trace = (value) => {
-  console.log(new Date());
-  console.log(`TRACE: ${value}`)
-}
+module.exports = Loggernaut;
